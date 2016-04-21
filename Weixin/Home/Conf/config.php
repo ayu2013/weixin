b6805bc2d8ba756dc92a5c7486aa091ce8e2e7ce
dingjian <139087006@qq.com>
2016-04-21 13:53:36 +0800

    更新

--- Weixin/Home/Conf/config.php
+++ Weixin/Home/Conf/config.php
@@ -9,6 +9,8 @@ return array(
     'DB_PORT'=>'3306',
     //微信配置文件
     'WX_TOKEN'=>'weixin',
-    'WX_APPID'=>'你的微信APPID',
-    'WX_APPSECRET'=>'你的微信APPSCRET',
+    'WX_APPID'=>'wx80e3fb27b08918c1',
+    'WX_APPSECRET'=>'8fe504cbe457876dc156a41086229492',
+//    'WX_APPID'=>'你的微信APPID',
+//    'WX_APPSECRET'=>'你的微信APPSCRET',
 );
